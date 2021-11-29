fc6c35e912118ada6f61a4d776267eb6a02e5882
Goanna7007 <45587025+Goanna7007@users.noreply.github.com>
2021-11-29 14:39:30 +1100

    got the basics working

--- Dymo M10 Scale Python/Dymo_M10_Scale_Python.py
+++ Dymo M10 Scale Python/Dymo_M10_Scale_Python.py
@@ -6,25 +6,24 @@
 
 
 import pywinusb.hid as hid
-
+import sched
 
 
 def dymo_scale_handler(data):
-    weight
+    global weight
 
     if data[1] == 2: #if zeroed
         weight = 0
     else:
-        weight = data[4] + data[5] * 255    #get weight
+        weight = data[4] + data[5] * 256    #get weight
 
         if data[1] == 5:    #if negative, flip over 0
             weight *= -1
 
-        if data[2] == 11:   #if in OZ, convert to KG
+        if data[2] == 11:   #if in OZ, convert to g
             weight = int((weight * 2.834952))
 
-    weight = weight - tare;
-    weight_text.set(str(weight) + "g")
+    print(str(weight) + "g")
 
 
 def get_dymo_scale():
@@ -38,20 +37,17 @@ def setup_dymo_scale():
     global device
     device = get_dymo_scale()
     if device:
-        try:
-            device.open()
-            device.set_raw_data_handler(dymo_scale_handler)
-            dymo_check_loop()
-        finally:
-            None
+        device.open()
+        device.set_raw_data_handler(dymo_scale_handler)
+        dymo_check_loop()
     else:
-        weight_text.set("connect and turn on scale")
-        root.after(100, setup_dymo_scale)  # reschedule event in 2 seconds
+        print("Please connect and turn on scale.")
+        sched.scheduler(2, setup_dymo_scale)  # reschedule event in 2 seconds
 
 def dymo_check_loop():
     global device
     if device.is_plugged():
-        root.after(1000, dymo_check_loop)# reschedule event in 2 seconds
+        sched.scheduler(2, dymo_check_loop)# reschedule event in 2 seconds
     else:
         device.close()
         device = None
@@ -60,3 +56,4 @@ def dymo_check_loop():
 
 if __name__ == '__main__':
     setup_dymo_scale()
+    input('Press ENTER to quit')
