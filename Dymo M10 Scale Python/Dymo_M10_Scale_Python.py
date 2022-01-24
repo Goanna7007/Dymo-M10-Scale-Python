be4f0183a065d2fa6d91f35c21e833445a9b904e
Johannes Nicholas <45587025+Goanna7007@users.noreply.github.com>
2022-01-24 18:29:38 +1100

    Added more comments

--- Dymo M10 Scale Python/Dymo_M10_Scale_Python.py
+++ Dymo M10 Scale Python/Dymo_M10_Scale_Python.py
@@ -9,8 +9,9 @@ import pywinusb.hid as hid
 import sched
 
 
+#called everytime data is received from scale
 def dymo_scale_handler(data):
-    global weight
+    global weight #grams on the scale
 
     if data[1] == 2: #if zeroed
         weight = 0
@@ -26,6 +27,7 @@ def dymo_scale_handler(data):
     print(str(weight) + "g")
 
 
+#find and return the scale
 def get_dymo_scale():
     all_hids = hid.find_all_hid_devices()
     for device in all_hids:
@@ -33,10 +35,12 @@ def get_dymo_scale():
             return device
     return None
 
+
+#setup the usb device listner
 def setup_dymo_scale():
-    global device
+    global device #The usb scale device
     device = get_dymo_scale()
-    if device:
+    if device:  #if the scale is connected
         device.open()
         device.set_raw_data_handler(dymo_scale_handler)
         dymo_check_loop()
@@ -44,11 +48,14 @@ def setup_dymo_scale():
         print("Please connect and turn on scale.")
         sched.scheduler(2, setup_dymo_scale)  # reschedule event in 2 seconds
 
+        
+#slowly loops while device is connected
 def dymo_check_loop():
     global device
     if device.is_plugged():
         sched.scheduler(2, dymo_check_loop)# reschedule event in 2 seconds
     else:
+        #the device is not currently connected, so try again
         device.close()
         device = None
         setup_dymo_scale()
